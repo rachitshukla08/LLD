7566aff240ad164b5aa6324c907d67d1353e5533
rachitshukla08 <rachitshukla08@gmail.com>
2024-06-04 01:07:36 +0530

    Added singleton pattern

---
+++ src/designpatterns/hard/three_singleton/EmployeeStore.java
@@ -0,0 +1,34 @@
+package designpatterns.hard.three_singleton;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EmployeeStore {
+
+    private final Map<String, String> employees;
+    private static EmployeeStore INSTANCE = null;
+    private EmployeeStore(){
+        this.employees = new HashMap<>();
+        // read DB and put in map
+        employees.put("101","Rachit1");
+        employees.put("102","Rachit2");
+        employees.put("103","Rachit3");
+    }
+
+    public synchronized static EmployeeStore getInstance() {
+        //Lazy initialization, need to put thread safety here therefore sync method
+        // In this case, this method can become bottleneck. To prevent it I can remove sync from method
+        // and do eager initialization. BUT:
+        // If i did eager initialization here (while declaring variable at top) then the initial
+        // load time would be higher
+        if(INSTANCE == null)
+            INSTANCE = new EmployeeStore();
+        return INSTANCE;
+    }
+
+    public String getName(String id) {
+        if(!this.employees.containsKey(id))
+            throw new IllegalArgumentException("No employee with this id");
+        return employees.get(id);
+    }
+}
